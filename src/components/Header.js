0b8f335ac6be880e9c553dde0cdc5dfd5202f546
Matheus Augusto <matheus_augusto92@hotmail.com>
2023-05-16 21:47:58 -0300

    requisito 4 v1.0

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -23,6 +24,15 @@ class Header extends Component {
       (loading) ? <Loading /> : (
         <div data-testid="header-component">
           <p data-testid="header-user-name">{ userName }</p>
+          <nav>
+            <ul>
+              <li><Link data-testid="link-to-search" to="/search">Search</Link></li>
+              <li>
+                <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+              </li>
+              <li><Link data-testid="link-to-profile" to="/profile">Profile</Link></li>
+            </ul>
+          </nav>
         </div>
       )
     );
