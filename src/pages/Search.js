ff9575c70864dd5f0b78daac425351a70dbe7b0f
Matheus Augusto <matheus_augusto92@hotmail.com>
2023-05-16 22:30:59 -0300

    requisito 5 v1.0

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,51 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    searchArtist: '',
+  };
+
+  handleNameChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { searchArtist } = this.state;
+    const minlengthSearchArtist = 2;
     return (
       <div data-testid="page-search">
         <Header />
         <h2>Search</h2>
+        <fieldset>
+          <legend>Pesquise uma Banda ou Artista</legend>
+          <br />
+          <label>
+            Digite Banda/Artista
+            <input
+              data-testid="search-artist-input"
+              id="searchArtist"
+              type="text"
+              name="searchArtist"
+              value={ searchArtist }
+              onChange={ this.handleNameChange }
+            />
+          </label>
+          <br />
+          <br />
+          <button
+            data-testid="search-artist-button"
+            id="searchArtistBtn"
+            type="submit"
+            name="searchArtistBtn"
+            // onClick={ }
+            disabled={ searchArtist.length < minlengthSearchArtist }
+          >
+            Pesquisar
+          </button>
+        </fieldset>
       </div>
     );
   }
